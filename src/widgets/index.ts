a89b751c6c9e29ee28f17797b42a3606f6471920
elhoim <elhoim@gmail.com>
2026-07-21 21:22:33 +0000

    feat(widgets): register rclone-queue widget

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -85,3 +85,4 @@ export { SandboxStatusWidget } from './SandboxStatus';
 export { VoiceStatusWidget } from './VoiceStatus';
 export { RemoteControlStatusWidget } from './RemoteControlStatus';
 export { CacheTimerWidget } from './CacheTimer';
+export { RCloneQueueWidget } from './RCloneQueue';
